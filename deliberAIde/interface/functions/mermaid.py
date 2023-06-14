98e95a8640f958798a46b7a96cc5d3328645089c
colmofu <cofuarthain@gmail.com>
2023-06-14 14:00:09 +0200

    Working diagrams

--- deliberAIde/interface/functions/mermaid.py
+++ deliberAIde/interface/functions/mermaid.py
@@ -54,27 +54,99 @@ def views_json_to_mermaid_mindmap(output: dict):
     return mermaid_graphs
 
 #Arguments mindmap - working well and displaying arguments for sub-viewpoints
+# def args_json_to_mermaid_mindmap(output: dict):
+#     mermaid_graphs = []
+
+#     def process_viewpoints(viewpoints, indent='    '):
+#         mermaid_graph = ''
+#         for viewpoint in viewpoints:
+#             if isinstance(viewpoint, str):
+#                 mermaid_graph += f'{indent}{viewpoint}\n'
+#             elif isinstance(viewpoint, dict):
+#                 viewpoint_text = viewpoint.get('viewpoint') or viewpoint.get('sub_viewpoint')
+#                 if viewpoint_text:
+#                     mermaid_graph += f'{indent}{viewpoint_text}\n'
+#                     arguments = viewpoint.get('arguments')
+#                     if arguments:
+#                         if isinstance(arguments, list):
+#                             for argument in arguments:
+#                                 mermaid_graph += f'{indent}    [{argument}]\n'
+#                         else:
+#                             mermaid_graph += f'{indent}    [{arguments}]\n'
+#                     sub_viewpoints = viewpoint.get('sub_viewpoints') or []
+#                     mermaid_graph += process_viewpoints(sub_viewpoints, indent + '    ')
+#         return mermaid_graph
+
+#     if 'main_topics' in output:
+#         main_topics = output.get('main_topics')
+#         for topic in main_topics:
+#             topic_text = topic.get('topic')
+#             if topic_text:
+#                 mermaid_graph = f'mindmap\n  root(({topic_text}))\n'
+#                 viewpoints = topic.get('viewpoints') or []
+#                 mermaid_graph += process_viewpoints(viewpoints)
+#                 mermaid_graph += '\n'
+#                 mermaid_graphs.append(mermaid_graph)
+#     else:
+#         main_topic = output.get('main_topic')
+#         if main_topic:
+#             mermaid_graph = f'mindmap\n  root(({main_topic}))\n'
+#             viewpoints = output.get('viewpoints') or []
+#             mermaid_graph += process_viewpoints(viewpoints)
+#             mermaid_graph += '\n'
+#             mermaid_graphs.append(mermaid_graph)
+#     return mermaid_graphs
+
 def args_json_to_mermaid_mindmap(output: dict):
     mermaid_graphs = []
 
     def process_viewpoints(viewpoints, indent='    '):
         mermaid_graph = ''
-        for viewpoint in viewpoints:
-            if isinstance(viewpoint, str):
-                mermaid_graph += f'{indent}{viewpoint}\n'
-            elif isinstance(viewpoint, dict):
-                viewpoint_text = viewpoint.get('viewpoint') or viewpoint.get('sub_viewpoint')
-                if viewpoint_text:
-                    mermaid_graph += f'{indent}{viewpoint_text}\n'
-                    arguments = viewpoint.get('arguments')
-                    if arguments:
-                        if isinstance(arguments, list):
-                            for argument in arguments:
-                                mermaid_graph += f'{indent}    [{argument}]\n'
-                        else:
-                            mermaid_graph += f'{indent}    [{arguments}]\n'
-                    sub_viewpoints = viewpoint.get('sub_viewpoints') or []
-                    mermaid_graph += process_viewpoints(sub_viewpoints, indent + '    ')
+        if isinstance(viewpoints, dict):
+            for viewpoint, contents in viewpoints.items():
+                if isinstance(viewpoint, str):
+                    mermaid_graph += f'{indent}({viewpoint})\n'
+                    arguments = contents.get('arguments', [])
+                    for argument in arguments:
+                        mermaid_graph += f'{indent}    ({argument})\n'
+                elif isinstance(viewpoint, dict):
+                    viewpoint_text = viewpoint.get('viewpoint') or viewpoint.get('sub_viewpoint')
+                    if viewpoint_text:
+                        mermaid_graph += f'{indent}({viewpoint_text})\n'
+                        arguments = viewpoint.get('arguments')
+                        if arguments:
+                            if isinstance(arguments, list):
+                                for argument in arguments:
+                                    summary = argument.get('summary')
+                                    if summary:
+                                        mermaid_graph += f'{indent}    ({summary})\n'
+                            else:
+                                summary = arguments.get('summary')
+                                if summary:
+                                    mermaid_graph += f'{indent}    ({summary})\n'
+                        sub_viewpoints = viewpoint.get('sub_viewpoints') or []
+                        mermaid_graph += process_viewpoints(sub_viewpoints, indent + '    ')
+        elif isinstance(viewpoints, list):
+            for viewpoint in viewpoints:
+                if isinstance(viewpoint, str):
+                    mermaid_graph += f'{indent}({viewpoint})\n'
+                elif isinstance(viewpoint, dict):
+                    viewpoint_text = viewpoint.get('viewpoint') or viewpoint.get('sub_viewpoint')
+                    if viewpoint_text:
+                        mermaid_graph += f'{indent}({viewpoint_text})\n'
+                        arguments = viewpoint.get('arguments')
+                        if arguments:
+                            if isinstance(arguments, list):
+                                for argument in arguments:
+                                    summary = argument.get('summary')
+                                    if summary:
+                                        mermaid_graph += f'{indent}    ({summary})\n'
+                            else:
+                                summary = arguments.get('summary')
+                                if summary:
+                                    mermaid_graph += f'{indent}    ({summary})\n'
+                        sub_viewpoints = viewpoint.get('sub_viewpoints') or []
+                        mermaid_graph += process_viewpoints(sub_viewpoints, indent + '    ')
         return mermaid_graph
 
     if 'main_topics' in output:
--- deliberAIde/interface/static/js/main.js
+++ deliberAIde/interface/static/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var socket = io.connect('http://localhost:5000/');  // Connect to the server-side socket. Adjust the URL as necessary.
+    var socket = io.connect();  // Connect to the server-side socket. Adjust the URL as necessary.
 
     $('.main-button').click(function(e){
         e.preventDefault();
@@ -20,77 +20,69 @@ $(document).ready(function(){
         socket.emit('button_called', data);
 
         // Set the min-height property of the #output div
-        $('#output').css('min-height', '500px');
+        $('#output').css('min-height', '400px');
+        $('#status-message').text('Generating topics...');
     });
 
     // Listen for update events
     socket.on('update', function(data) {
         console.log("Received data: ", data);
         // Do something with the data. This depends on the structure of your data.
-        if(data.topics) {
-            var topicsDiv = $('<div>');
-            // append topics to the output div
-            var topicsHTML = '<br><h2> Here are the topics:</h2>';
-            topicsHTML += '<p>' + data.topics.main_topic + '</p>';
-            topicsDiv.html(topicsHTML);
-            //$('#output').append(topicsHTML);
+        if (data.topics_mindmap) {
+            console.log('topics mindmap detected');
+            // Append topics mindmap to the output div
+            var topicsDiv = $('<div class="mermaid">');
+            topicsDiv.text(data.topics_mindmap);
             topicsDiv.hide().appendTo('#output').fadeIn(1000);
-            console.log('topics appended');
+            mermaid.init(undefined, topicsDiv);
+            // Check if the mindmap is initialized
+            console.log('topics mindmap initialized');
+            if (data.viewpoints_mindmap) {
+                $('#status-message').text('Generating viewpoints...');
+            }
+            else if (data.arguments_mindmap) {
+                $('#status-message').text('Generating arguments...');
+            }
+            else {
+                $('#status-message').text('Completed.');
+            }
         }
-        if(data.viewpoints) {
-            // Append viewpoints to the output div
-            var viewpointsDiv = $('<div>');
-            var viewpointsHTML = '<br><h2> Here are the viewpoints:</h2>';
-        
-            // Iterate over the viewpoints array
-            $.each(data.viewpoints.viewpoints, function(index, viewpointObj){
-                // Access viewpoint and append to HTML
-                var viewpoint = viewpointObj.viewpoint;
-                console.log("viewpoint here is: ", viewpoint);
-                viewpointsHTML += '<p>' + viewpoint + '</p>';
-                
-                // Check for any sub-viewpoints and append
-                var subViewpoints = viewpointObj.sub_viewpoints;
-                $.each(subViewpoints, function(index, subViewpointObj){
-                    var subViewpoint = subViewpointObj.viewpoint;
-                    console.log("Subviewpoint here is: ", subViewpoint);
-                    viewpointsHTML += '<p>---' + subViewpoint + '</p>';
-                });
-            });
-            
-            viewpointsDiv.html(viewpointsHTML);
-            //$('#output').append(viewpointsHTML);
+    
+        if (data.viewpoints_mindmap) {
+            $('#output').html('');  // Clear the output div
+            console.log('viewpoints mindmap detected');
+            // Append topics mindmap to the output div
+            var viewpointsDiv = $('<div class="mermaid">');
+            viewpointsDiv.text(data.viewpoints_mindmap);
+            console.log(viewpointsDiv);
             viewpointsDiv.hide().appendTo('#output').fadeIn(1000);
-            //$('#output').hide().fadeIn(1000);  // Add this line
-            console.log('viewpoints appended');
-        }   
-        if(data.arguments) {
-            // Append arguments to the output div
-            var argumentsHTML = '<br><h2> Here are the arguments:</h2>';
-            var argumentsDiv = $('<div>');
-            // Iterate over the viewpoints array
-            $.each(data.arguments.viewpoints, function(index, viewpointObj){
-                // Access viewpoint
-                //var viewpoint = viewpointObj.viewpoint;
-                // console.log("viewpoint here is: ", viewpoint);
-                // argumentsHTML += '<p><strong>' + viewpoint + '</strong></p>';
-                
-                // Access and append each argument for the viewpoint
-                var arguments = viewpointObj.arguments;
-                $.each(arguments, function(index, argumentObj){
-                    var argumentSummary = argumentObj.summary;
-                    console.log("Argument summary here is: ", argumentSummary);
-                    argumentsHTML += '<p>-- ' + argumentSummary + '</p>';
-                });
-            });
-            argumentsDiv.html(argumentsHTML);  
-            //$('#output').append(argumentsHTML);
-            argumentsDiv.hide().appendTo('#output').fadeIn(1000);
-            //$('#output').hide().fadeIn(1000);  // Add this line for fade in
-            console.log('arguments appended');
+            mermaid.init(undefined, viewpointsDiv);
+            // Check if the mindmap is initialized
+            console.log('viewpoints mindmap initialized');
+            if (data.arguments_mindmap) {
+                $('#status-message').text('Generating arguments...');
+            }
+            else {
+                $('#status-message').text('Completed.');
+            }
         }
-        //var output = document.getElementById('output'); Old scroll
-        //output.scrollIntoView({behavior: "smooth"});
+
+        if (data.arguments_mindmap) {
+            $('#output').html('');  // Clear the output div
+            console.log('arguments mindmap detected');
+            console.log(data.arguments_mindmap);
+            // Append topics mindmap to the output div
+            var argumentsDiv = $('<div class="mermaid">');
+            argumentsDiv.text(data.arguments_mindmap);
+            console.log(argumentsDiv);
+            argumentsDiv.hide().appendTo('#output').fadeIn(1000);
+            mermaid.init(undefined, argumentsDiv);
+            // Check if the mindmap is initialized
+            console.log('arguments mindmap initialized');
+            $('#status-message').text('Completed.')
+        }
+
+
         $('html, body').animate({
             scrollTop: $("#output").offset().top
         }, 2000); // 2000 milliseconds for scrolling
@@ -101,3 +93,71 @@ $(document).ready(function(){
     });
     });
 });
+
+
+        //var output = document.getElementById('output'); Old scroll
+        //output.scrollIntoView({behavior: "smooth"});
+
+
+        // if(data.topics) {
+        //     var topicsDiv = $('<div>');
+        //     // append topics to the output div
+        //     var topicsHTML = '<br><h2> Here are the topics:</h2>';
+        //     topicsHTML += '<p>' + data.topics.main_topic + '</p>';
+        //     topicsDiv.html(topicsHTML);
+        //     topicsDiv.hide().appendTo('#output').fadeIn(1000);
+
+        //     console.log('topics as a list appended');
+        // }
+
+        // if(data.viewpoints) {
+        //     // Append viewpoints to the output div
+        //     var viewpointsDiv = $('<div>');
+        //     var viewpointsHTML = '<br><h2> Here are the viewpoints:</h2>';
+        
+        //     // Iterate over the viewpoints array
+        //     $.each(data.viewpoints.viewpoints, function(index, viewpointObj){
+        //         // Access viewpoint and append to HTML
+        //         var viewpoint = viewpointObj.viewpoint;
+        //         console.log("viewpoint here is: ", viewpoint);
+        //         viewpointsHTML += '<p>' + viewpoint + '</p>';
+                
+        //         // Check for any sub-viewpoints and append
+        //         var subViewpoints = viewpointObj.sub_viewpoints;
+        //         $.each(subViewpoints, function(index, subViewpointObj){
+        //             var subViewpoint = subViewpointObj.viewpoint;
+        //             console.log("Subviewpoint here is: ", subViewpoint);
+        //             viewpointsHTML += '<p>---' + subViewpoint + '</p>';
+        //         });
+        //     });
+            
+        //     viewpointsDiv.html(viewpointsHTML);
+        //     viewpointsDiv.hide().appendTo('#output').fadeIn(1000);
+
+        //     // Check if viewpoints are appended
+        //     console.log('viewpoints appended');
+        // if(data.arguments) {
+        //     // Append arguments to the output div
+        //     var argumentsHTML = '<br><h2> Here are the arguments:</h2>';
+        //     var argumentsDiv = $('<div>');
+        //     // Iterate over the viewpoints array
+        //     $.each(data.arguments.viewpoints, function(index, viewpointObj){
+        //         // Access viewpoint
+        //         //var viewpoint = viewpointObj.viewpoint;
+        //         // console.log("viewpoint here is: ", viewpoint);
+        //         // argumentsHTML += '<p><strong>' + viewpoint + '</strong></p>';
+                
+        //         // Access and append each argument for the viewpoint
+        //         var arguments = viewpointObj.arguments;
+        //         $.each(arguments, function(index, argumentObj){
+        //             var argumentSummary = argumentObj.summary;
+        //             console.log("Argument summary here is: ", argumentSummary);
+        //             argumentsHTML += '<p>-- ' + argumentSummary + '</p>';
+        //         });
+        //     });
+        //     argumentsDiv.html(argumentsHTML);  
+        //     //$('#output').append(argumentsHTML);
+        //     argumentsDiv.hide().appendTo('#output').fadeIn(1000);
+        //     //$('#output').hide().fadeIn(1000);  // Add this line for fade in
+        //     console.log('arguments appended');
+        // }
